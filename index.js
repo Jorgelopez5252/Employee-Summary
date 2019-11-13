1c29eb2dbdc9f46e8ed7b964e51494d488c91c44
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-11-12 17:48:52 -0700

    updated render templates in htmlrender folder

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const Engineer = require("./lib/constructors/Engineer");
 const Intern = require("./lib/constructors/Intern");
 
 // Global Variables
-const teamMembers = [
-  new Manager("Brent", 1, "brent@trilogyed.com", 200),
-  new Engineer("Tucker", 2, "tbeauchamp@2u.com", "tuckerbeauchamp"),
-  new Intern("Becky", 3, "becky@becky.com", "UofA"),
-  new Intern("Shelly", 4, "shelly@shelly.com", "UofA")
-];
+// const teamMembers = [
+//   new Manager("Brent", 1, "brent@trilogyed.com", 200),
+//   new Engineer("Tucker", 2, "tbeauchamp@2u.com", "tuckerbeauchamp"),
+//   new Intern("Becky", 3, "becky@becky.com", "UofA"),
+//   new Intern("Shelly", 4, "shelly@shelly.com", "UofA")
+// ];
+
+const teamMembers =[];
 /***
  * THIS PROJECT IS NOT COMPLETE.
  * YOU WILL NEED TO BUILD YOUR HTML LAYOUTS
